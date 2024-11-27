b384d1d4fd31327d79f986597306581d685a179c
tanvia09 <136640828+tanvia09@users.noreply.github.com>
2024-11-26 20:26:40 -0500

    Added time input

--- react-install/src/pages/Tasks.js
+++ react-install/src/pages/Tasks.js
@@ -1,11 +1,20 @@
 import Header from '../components/Header'
 import 'react-calendar/dist/Calendar.css';
 
-
 export default function Tasks() {
+
     return (
         <div>
             <Header />
+            <h2>Create a task</h2>
+            <label for="taskName">Task: </label>
+            <input type="text" id="taskName" name="taskName" required/>
+            <label for="time">Time:</label>
+            <input type="datetime-local" id="taskTime" name="taskTime" required/>
+            <button type="submit">Submit</button>
+
+            <h2>Tasks:</h2>
+            <ul id="taskList"></ul>
         </div>
     )
 }
